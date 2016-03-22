da8f9089ab15e68c1eefd98c79c536a2ba37f2c1
HarveenS <ronith19>
2016-03-22 20:55:27 +1100

    done so far

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('body.charlie').change('url(img/charlie-1.1.jpg)'){
+		$('.background').css('url(img/charlie2nd.jpg)')};
+	
+});
+	
+	//function func() {
+      //  document.body.charlie.id = "background"+int;
+        //int++;
+        //if(int === frames) { int = 1; }
+   
+    
+   // var swap = window.setInterval(func, interval);
+// }
+
+// run(5000, 2); //milliseconds, frames
+// }
+
+	
+	//$('.data-book1').click(function() {
+	//	$('.modal-content').css('display', 'block');
+	//$('.modal-content').click(function() {
+	//	$('.modal-content').css('display', 'none');
